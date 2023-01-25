f3a92701964e6a30ea826e8ec42078761d7928c8
Zyre  Mendoza <82299436+zyre-mendoza@users.noreply.github.com>
2023-01-25 13:30:21 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import data from "./data"
+
+ function App() {
+    const cards = data.map(item => {
+        return (
+            <Card
+                key={item.id}
+                item={item}
+            />
+        )
+    })        
+    
+            // <Hero />
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+            <section className="cards-list">
+                {cards}
+            </section>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+
+
+export default function Navbar() {
+    return (
+        <nav>
+            <div className="logo">
+               <a href="#"><h1>Kharla.</h1></a>
+            </div>
+
+            <div className="links">
+                <a>Home</a>
+                <a>About</a>
+                <a>Resume</a>
+                <a>Contact</a>
+            </div>
+        </nav>
+    )
+}
